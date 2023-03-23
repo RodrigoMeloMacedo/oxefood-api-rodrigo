625ad2df90c6ff9308e7754b2db0b115037f4b01
Rodrigo <ramm@discente.ifpe.edu.br>
2023-03-23 20:12:39 -0300

    versão 2 - criado classes

---
+++ src/main/java/oxefoodapirodrigo/oxefoodapirodrigo/OxefoodApiRodrigoApplication.java
@@ -0,0 +1,13 @@
+package oxefoodapirodrigo.oxefoodapirodrigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxefoodApiRodrigoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxefoodApiRodrigoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oxefoodapirodrigo/oxefoodapirodrigo/OxefoodApiRodrigoApplicationTests.java
@@ -0,0 +1,13 @@
+package oxefoodapirodrigo.oxefoodapirodrigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OxefoodApiRodrigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
